59112e6ce6fccf7094939b70455fa0eea43b6b8b
Winlypse <nts3@o2.pl>
2024-10-22 15:34:51 +0200

    Changes!

--- src/main/kotlin/winlyps/noDrowningDamage/NoDrowningDamage.kt
+++ src/main/kotlin/winlyps/noDrowningDamage/NoDrowningDamage.kt
@@ -6,9 +6,14 @@ class NoDrowningDamage : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("drowningDamage", "false")
+        }
+        logger.info("NoDrowningDamage plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoDrowningDamage plugin has been disabled.")
     }
 }
